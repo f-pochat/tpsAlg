fdf1e2202b92754670baf808eca25d4cc7cefd18
fpochat02 <fpochat@losmolinos.edu.ar>
2021-03-25 10:48:51 -0300

    Stack & Queues 2

--- Tps/StacksandQueues/QueueEst.java
+++ Tps/StacksandQueues/QueueEst.java
@@ -1,28 +1,67 @@
 package StacksandQueues;
 
-public class QueueEst implements QueueInterface{
+public class QueueEst implements QueueInterface {
 
     Object[] queue;
-    int
+    private int arraySize;
+    private int size;
+    int front;
+    int end;
 
+    public QueueEst() {
+        queue = new Object[10];
+        arraySize = 10;
+        size = 0;
+        front = 0;
+        end = 0;
+    }
 
     @Override
     public void enqueue(Object element) {
+        if (end < arraySize){
+            queue[end] = element;
+            end++;
+        }else {
+            if (front > 0){
+                end = 0;
+                queue[end] = element;
+                end++;
+            }else {
+                enlargeQueue();
+                enqueue(element);
+            }
+        }
 
+        size++;
+    }
+
+    private void enlargeQueue() {
+        Object[] queueAux = new Object[size() + 10];
+        for (int i = 0; i < size(); i++) {
+            queueAux[i] = queue[i];
+        }
+        queue = queueAux;
+        arraySize += 10;
     }
 
     @Override
     public Object dequeue() throws IsEmptyException {
-        return null;
+        if (isEmpty()){
+            throw new IsEmptyException("Queue is Empty");
+        }else {
+            size--;
+            front++;
+            return queue[front-1];
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 }
